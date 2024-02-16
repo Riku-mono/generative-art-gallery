68374c5cc53d0d5f8ec19db0d3afec8503627d9f
Riku-mono <riitkou@gmail.com>
2024-02-16 17:26:42 +0900

    Add Markdown Component

---
+++ src/components/markdown/Markdown.tsx
@@ -0,0 +1,44 @@
+import ReactMarkdown from 'react-markdown'
+import 'highlight.js/styles/github-dark.css'
+import RehypeHighlight from 'rehype-highlight'
+import RemarkGfm from 'remark-gfm'
+
+export default function Markdown({ markdown }: { markdown: string }) {
+  return (
+    <div className="w-full overflow-hidden break-words rounded-md bg-neutral-50 p-4 dark:bg-neutral-800 sm:bg-neutral-100 sm:p-6 lg:p-8">
+      <ReactMarkdown
+        remarkPlugins={[RemarkGfm]}
+        rehypePlugins={[RehypeHighlight]}
+        className="prose dark:prose-invert max-w-none"
+        components={{
+          h1: ({ node, children }) => <h1 className="dark:border-gray-700">{children}</h1>,
+          h2: ({ node, children }) => <h2 className="dark:border-gray-700">{children}</h2>,
+          code: ({ node, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+              <>
+                <span className="absolute left-0 top-0 w-full select-none border-b border-neutral-500 bg-slate-700 px-2 py-0.5 text-sm dark:bg-neutral-700">
+                  {match[1]}
+                </span>
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              </>
+            ) : (
+              <code className={`dark:bg-neutral-700`} {...props}>
+                {children}
+              </code>
+            )
+          },
+          table: ({ children }) => (
+            <div className="overflow-x-auto">
+              <table>{children}</table>
+            </div>
+          ),
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,64 @@ const config: Config = {
   darkMode: 'class',
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontSize: '1.875rem',
+              lineHeight: '2.25rem',
+              fontWeight: '700',
+              paddingBottom: '0.5rem',
+              borderBottom: '1px solid #e2e8f0',
+            },
+            h2: {
+              fontWeight: '600',
+              paddingBottom: '0.5rem',
+              borderBottom: '1px solid #e2e8f0',
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            h4: {
+              fontWeight: '600',
+            },
+            h5: {
+              fontWeight: '600',
+            },
+            table: {
+              width: 'auto',
+              'border-collapse': 'collapse',
+              'overflow-x': 'auto',
+            },
+            pre: {
+              position: 'relative',
+            },
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+            code: {
+              padding: '0.2em 0.4em',
+              background: '#e2e8f0',
+              borderRadius: '0.25rem',
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+          },
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
 export default config
