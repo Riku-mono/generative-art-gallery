d02c265a69ad86656b9b31cfa4070d7adcbe3cfc
Riku-mono <riitkou@gmail.com>
2024-02-23 01:24:17 +0900

    Update generator availability

--- src/app/art/[id]/generator/page.tsx
+++ src/app/art/[id]/generator/page.tsx
@@ -56,7 +56,26 @@ export default async function ArtGeneratorPage({ params }: { params: { id: strin
         className="mx-auto my-4 flex max-w-full flex-col gap-y-4 px-2"
         style={{ width: canvasSize.width }}
       >
-        <Generator id={params.id} canvasSize={canvasSize} type={detail.generatorType} />
+        {detail.generate && (
+          <Generator id={params.id} canvasSize={canvasSize} type={detail.generatorType} />
+        )}
+        {!detail.generate && (
+          <>
+            <div className="text-center text-xl text-neutral-600 dark:text-neutral-400">
+              Generator is not available
+            </div>
+            <div className="text-center text-neutral-500 dark:text-neutral-300">
+              Please check{' '}
+              <a
+                href={`https://github.com/Riku-mono/generative-art-gallery/tree/main/public/ArtData`}
+                className="underline"
+                target="_blank"
+              >
+                here
+              </a>
+            </div>
+          </>
+        )}
       </div>
     </>
   )
