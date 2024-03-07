d1072424b44b63820b224ed46d3520ee5bca2f9f
Riku-mono <riitkou@gmail.com>
2024-03-08 02:47:30 +0900

    Add thumbnail image to the header of art details

--- src/app/art/[id]/layout.tsx
+++ src/app/art/[id]/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { getDetail, getFolderPaths } from '@/app/api/fs'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
@@ -48,12 +49,23 @@ export default async function Artlayout({
     <>
       <main className="relative mx-auto my-8 flex w-full max-w-5xl flex-1 flex-col gap-y-4">
         <div className="px-6">
-          <h1 className="mb-2 text-5xl font-bold text-neutral-600 dark:text-neutral-300">
-            #{params.id}
-          </h1>
-          <h2 className="text-3xl font-bold text-neutral-900 dark:text-neutral-50">
-            {detail.title}
-          </h2>
+          <div className="mb-2 flex justify-between gap-2">
+            <div>
+              <h1 className="mb-2 text-5xl font-bold text-neutral-600 dark:text-neutral-300">
+                #{params.id}
+              </h1>
+              <h2 className="text-3xl font-bold text-neutral-900 dark:text-neutral-50">
+                {detail.title}
+              </h2>
+            </div>
+            <Image
+              src={`/ArtData/${params.id}/images/thumbnail.webp`}
+              alt={`${params.id}-thumbnail`}
+              width={96}
+              height={96}
+              className="mb-auto w-24 rounded-md border-2 border-neutral-200 bg-neutral-100 object-contain dark:border-neutral-700"
+            />
+          </div>
           <MultiLineText>{detail.description}</MultiLineText>
           <div className="flex flex-wrap gap-x-2 text-sm text-neutral-500 dark:text-neutral-400">
             <span>API & Libraly: </span>
@@ -71,20 +83,20 @@ export default async function Artlayout({
           </p>
         </div>
         <div className="px-6">{children}</div>
-        <nav className="flex justify-center px-10">
+        <nav className="flex justify-center gap-2 px-10">
           {(prevPath && (
-            <a href={`/art/${prevPath}`} className="mr-2">
-              <button className="btn underline">Prev</button>
+            <a href={`/art/${prevPath}`} className="underline">
+              Prev
             </a>
-          )) || <div className="mr-2 text-neutral-400">Prev</div>}
-          <a href="/">
-            <button className="btn underline">Home</button>
+          )) || <div className="text-neutral-400">Prev</div>}
+          <a href="/" className="underline">
+            Home
           </a>
           {(nextPath && (
-            <a href={`/art/${nextPath}`} className="ml-2">
-              <button className="btn underline">Next</button>
+            <a href={`/art/${nextPath}`} className="underline">
+              Next
             </a>
-          )) || <div className="ml-2 text-neutral-400 ">Next</div>}
+          )) || <div className="text-neutral-400 ">Next</div>}
         </nav>
       </main>
     </>
