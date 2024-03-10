54a1390b05f0efea2f423710cfb78b44d2d77e1a
Riku-mono <riitkou@gmail.com>
2024-03-11 02:34:37 +0900

    Refactor overlapping Navigation

--- src/app/art/[id]/examples/page.tsx
+++ src/app/art/[id]/examples/page.tsx
@@ -1,4 +1,5 @@
 import { getFilePaths } from '@/app/api/fs'
+import ArtNavigation from '@/components/art/Navigation'
 import Image from 'next/image'
 
 export default async function ArtExamplesPage({ params }: { params: { id: string } }) {
@@ -10,50 +11,9 @@ export default async function ArtExamplesPage({ params }: { params: { id: string
     images.unshift(thumbnail)
   }
 
-  let targets = [
-    {
-      target: '',
-      label: 'Info',
-      isActive: false,
-    },
-    {
-      target: 'examples',
-      label: 'Examples',
-      isActive: true,
-    },
-    {
-      target: 'generator',
-      label: 'Generator',
-      isActive: false,
-    },
-  ]
-
   return (
     <>
-      <div className="flex flex-wrap gap-4">
-        {targets.map((target, index) =>
-          target.isActive ? (
-            <div
-              key={index}
-              className="bg-primary-500 rounded-md bg-neutral-700 px-4 py-2 text-neutral-100 dark:bg-neutral-200 dark:text-neutral-600"
-            >
-              {target.label}
-            </div>
-          ) : (
-            <a
-              key={index}
-              href={`/art/${params.id}/${target.target}`}
-              className={`${
-                target.isActive
-                  ? 'bg-primary-500 text-neutral-50'
-                  : 'bg-neutral-200 text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200'
-              } rounded-md px-4 py-2`}
-            >
-              {target.label}
-            </a>
-          )
-        )}
-      </div>
+      <ArtNavigation parent={params.id} activeTarget="examples" />
       <div className="scrollbar-hide my-4 grid w-full grid-cols-1 gap-2 overflow-x-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {images.length === 0 && <div>No examples available</div>}
         {images.map((image, index) => (
--- src/app/art/[id]/page.tsx
+++ src/app/art/[id]/page.tsx
@@ -1,53 +1,13 @@
 import { getArticle } from '@/app/api/fs'
+import ArtNavigation from '@/components/art/Navigation'
 import Markdown from '@/components/markdown/Markdown'
 
 export default async function Art({ params }: { params: { id: string } }) {
   const markdown = await getArticle(`ArtData/${params.id}/article.md`)
 
-  let targets = [
-    {
-      target: '',
-      label: 'Info',
-      isActive: true,
-    },
-    {
-      target: 'examples',
-      label: 'Examples',
-      isActive: false,
-    },
-    {
-      target: 'generator',
-      label: 'Generator',
-      isActive: false,
-    },
-  ]
-
   return (
     <>
-      <div className="flex flex-wrap gap-4">
-        {targets.map((target, index) =>
-          target.isActive ? (
-            <div
-              key={index}
-              className="bg-primary-500 rounded-md bg-neutral-700 px-4 py-2 text-neutral-100 dark:bg-neutral-200 dark:text-neutral-600"
-            >
-              {target.label}
-            </div>
-          ) : (
-            <a
-              key={index}
-              href={`/art/${params.id}/${target.target}`}
-              className={`${
-                target.isActive
-                  ? 'bg-primary-500 text-neutral-50'
-                  : 'bg-neutral-200 text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200'
-              } rounded-md px-4 py-2`}
-            >
-              {target.label}
-            </a>
-          )
-        )}
-      </div>
+      <ArtNavigation parent={params.id} activeTarget="" />
       <div className="my-4">
         {(markdown === '' && (
           <div className="text-center text-xl text-neutral-600 dark:text-neutral-400">
